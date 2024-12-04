40293561ef5ec83f9e0e2c1ac430dc629de59398
lluisespert <lluisespertc@gmail.com>
2024-12-04 18:42:22 +0000

    Footer terminado

---
+++ src/js/component/Footer.js
@@ -0,0 +1,26 @@
+import react from "react";
+
+export const Footer = () => {
+
+    return (
+
+        <>
+
+        <hr/>
+
+              <p>
+
+                  <a href="https://github.com/bhgeeky/bhgeeky">Brian Hernández González</a>, with
+
+                  <a href="https://github.com/MarcosCarset">Marcos Caset</a>, with,
+
+                  <a href="https://github.com/lluisespert">Lluís Espert</a>.
+
+              </p>
+              
+      </>
+  
+
+    )
+
+}
